40e50e38c07c5e2accf2c45257266380969d3be7
James <james1239090@gmail.com>
2017-04-23 16:09:54 +0800

    generating an uploader and mount to Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :product, optional: true
-
+  mount_uploaders :images, PhotoUploader
+  serialize :images, JSON # If you use SQLite, add this line.
 end
