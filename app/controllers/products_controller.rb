6610ee2f84f4f495fa926204d2e34dc413b91115
James <james1239090@gmail.com>
2017-04-23 17:06:18 +0800

    implement destroy product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 
   private
   def product_params
